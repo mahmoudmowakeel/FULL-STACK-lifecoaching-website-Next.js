5cc15ff4705979ff6bec60743ab3da3e5a090d8b
m84388533 <mahmoud.el.wakeel@h-partners.com>
2025-11-03 13:39:50 +0200

    make contact icons clickable

--- src/app/[locale]/contact-us/page.tsx
+++ src/app/[locale]/contact-us/page.tsx
@@ -3,33 +3,51 @@ import Image from "next/image";
 export default function ContactUpPage() {
   return (
     <div className="w-full h-full flex flex-col justify-center gap-7 items-center">
-      <div dir="ltr" className="flex  items-center gap-2 justify-start">
+      <div dir="ltr" className="flex items-center gap-2 justify-start">
         <Image
           src="/Images/email.svg"
           width={30}
           height={30}
-          alt="snap"
+          alt="email"
           className="bg-[#214E78] px-2 py-2 rounded-full"
         />
-        <p className="text-md">Istifhamcompany@gmail.com</p>
+        <a
+          href="mailto:Istifhamcompany@gmail.com"
+          className="text-md hover:underline"
+        >
+          Istifhamcompany@gmail.com
+        </a>
       </div>
+
       <div className="flex flex-col justify-center items-center">
-        <div dir="ltr" className="flex  items-center gap-2">
-          <Image
-            src="/Images/phone.svg"
-            width={30}
-            height={30}
-            alt="snap"
-            className="bg-[#214E78] px-2 py-2 rounded-full"
-          />
-          <Image
-            src="/Images/whatsapp.svg"
-            width={30}
-            height={30}
-            alt="youtube"
-            className="bg-[#214E78] px-2 py-2 rounded-full"
-          />
-          <p className="text-md">+966545938783</p>
+        <div dir="ltr" className="flex items-center gap-2">
+          <a href="tel:+966545938783">
+            <Image
+              src="/Images/phone.svg"
+              width={30}
+              height={30}
+              alt="phone"
+              className="bg-[#214E78] px-2 py-2 rounded-full"
+            />
+          </a>
+
+          <a
+            href="https://wa.me/966545938783"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/Images/whatsapp.svg"
+              width={30}
+              height={30}
+              alt="whatsapp"
+              className="bg-[#214E78] px-2 py-2 rounded-full"
+            />
+          </a>
+
+          <a href="tel:+966545938783" className="text-md hover:underline">
+            +966545938783
+          </a>
         </div>
       </div>
     </div>
